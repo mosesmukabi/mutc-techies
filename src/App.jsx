cc557af175e5262f29f96656644b0d9be80578ca
moses mukabi <mosesmukabi9@gmai.com>
2024-09-25 12:08:04 -0500

    designed a footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Header from './components/Header/Header'
+import Footer from './components/Footer/footer'
 
 function App() {
 
   return (
     <>
     <Header />
-      
+    <Footer />
     </>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./footer.css"
+
+function Footer() {
+  return (
+    <footer className="footer" background-color="red">
+        <p className="footer-text">
+           &copy; 2024. All rights reserved. Well designed by <a href="https://github.com/mosesmukabi/">Moses Mukabi</a>
+        </p>
+         
+    </footer>
+  )
+}
+
+export default Footer
