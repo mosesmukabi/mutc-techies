97b582e177401a51342b22c3d943f6c7903a3808
moses mukabi <mosesmukabi9@gmai.com>
2024-09-26 05:18:28 -0500

    add the tittle componet

---
+++ src/components/Tittle/Tittle.jsx
@@ -0,0 +1,11 @@
+import "./Tittle.css"
+
+function Tittle({mainTittle}) {
+  return (
+    <div className='Tittle'>
+        <h1 className='Tittle__main'>{mainTittle}</h1>
+    </div>
+  )
+}
+
+export default Tittle
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Hero from '../../components/Hero/Hero'
+import About from '../../components/About/About'
+import Tittle from '../../components/Tittle/Tittle'
 function Home() {
   return (
     <>
     <Hero  />
+    <Tittle mainTittle ="About us"/>
+    <About />
+
     </>
   )
 }
