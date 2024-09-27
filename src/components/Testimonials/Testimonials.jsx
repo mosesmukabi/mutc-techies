d6eb8735fb337db4d3a4187eba1de4f22cd9e8ab
moses mukabi <mosesmukabi9@gmai.com>
2024-09-27 00:06:17 -0500

    designed the testimonial section

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,10 +1,7 @@
+
 import "./Testimonials.css"
-import james from '../../assets/james.jpg'
-import moses from '../../assets/james.jpg'
-import grcae from '../../assets/james.jpg'
-import newton from '../../assets/james.jpg'
-import peter from '../../assets/james.jpg'
-import isaac from '../../assets/james.jpg'
+import testmonies from "../../data/testmonies";
+import Carousel from 'react-elastic-carousel';
 
 function Testimonialscards({studentAvatar,studentName, title, text}) {
        return(
@@ -23,13 +20,24 @@ function Testimonials() {
   return (
     <section>
         <div className="testmonials-container">
-            <Testimonialscards studentAvatar={james} studentName="James Mwangi" title="A Life-Changing Experience" text="Joining MUTC was one of the best decisions I've made at university. The hands-on projects and supportive community helped me build skills I never thought I could develop." />
-            {/* <Testimonialscards studentAvatar={peter} studentName="Peter maina" title="A Launchpad for My Career" text="MUTC gave me the opportunity to work on real-world projects and collaborate with like-minded peers. Thanks to the club, I landed an internship that kickstarted my tech career."/>
-            <Testimonialscards studentAvatar={newton} studentName="newtone karanga" title="Innovative Learning Environment" text="The workshops and hackathons organized by MUTC are both fun and educational. I’ve learned so much about web development and cloud computing in a short time"/>
-            <Testimonialscards studentAvatar={grcae} studentName="Grace mbuthia" title="Supportive Leadership" text="The mentorship I received from MUTC's leadership was invaluable. They encouraged me to push my limits and pursue opportunities I wouldn’t have considered on my own."/>
-            <Testimonialscards studentAvatar={moses} studentName="moses mukabi" tittle="A Hub for Tech Enthusiasts " text="MUTC brings together students passionate about technology. The collaborative environment and shared knowledge have been instrumental in my growth as a developer"/>
-            <Testimonialscards studentAvatar={isaac} studentName="isaac karanja" title ="Empowering Student Innovators" text ="MUTC fosters creativity and innovation in every member. I have grown both technically and personally, and I now feel more confident pursuing a career in tech "/> */}
+     <Carousel  enableAutoPlay = {true} itemsToShow ={1} autoPlaySpeed="5000">
+         
+         {
+            testmonies.map((testmony, i)=>(
+                <Testimonialscards
+                key={i}
+                studentAvatar={testmony.studentAvatar}
+                studentName={testmony.studentName}
+                title={testmony.title}
+                text={testmony.text}
+
+                />
+            ))
+         }
+    </Carousel>
+        
         </div>
+
     </section>
   )
 }
---
+++ src/data/testmonies.js
@@ -0,0 +1,52 @@
+import james from '../assets/james.jpg'
+import moses from '../assets/moses.jpg'
+import grcae from '../assets/grace.jpg'
+import newton from '../assets/newton.jpg'
+import peter from '../assets/peter.jpg'
+import isaac from '../assets/isaac.jpg'
+
+const testmonies = [
+    {
+    studentAvatar:james,
+    studentName:"James Mwangi", 
+    title:"A Life-Changing Experience",
+    text:"Joining MUTC was one of the best decisions I've made at university. The hands-on projects and supportive community helped me build skills I never thought I could develop.",
+    },
+
+    {
+    studentAvatar:peter,
+    studentName:"Peter maina",
+    title:"A Launchpad for My Career",
+    text:"MUTC gave me the opportunity to work on real-world projects and collaborate with like-minded peers. Thanks to the club, I landed an internship that kickstarted my tech career.",
+   },
+   {
+     studentAvatar:newton,
+    studentName:"newtone karanga", 
+    title:"Innovative Learning Environment",
+    text:"The workshops and hackathons organized by MUTC are both fun and educational. I’ve learned so much about web development and cloud computing in a short time",
+},
+{
+    studentAvatar:grcae,
+    studentName:"Grace mbuthia",
+    title:"Supportive Leadership",
+    text:"The mentorship I received from MUTC's leadership was invaluable. They encouraged me to push my limits and pursue opportunities I wouldn’t have considered on my own.",
+},
+    {
+        studentAvatar:moses,
+        studentName:"moses mukabi",
+        title:"A Hub for Tech Enthusiasts",
+        text:"MUTC brings together students passionate about technology. The collaborative environment and shared knowledge have been instrumental in my growth as a developer",
+    },  
+          
+    {
+        studentAvatar:isaac,
+        studentName:"isaac karanja",
+        title :"Empowering Student Innovators",
+        text :"MUTC fosters creativity and innovation in every member. I have grown both technically and personally, and I now feel more confident pursuing a career in tech",
+    },
+            
+
+]
+
+
+export default testmonies
