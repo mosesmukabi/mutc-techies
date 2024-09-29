bbd845e569e1614c13f96b7ea2f254691775726c
moses mukabi <mosesmukabi9@gmai.com>
2024-09-29 08:13:59 -0500

    naming error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Header from "./components/Header/Header";
-import Footer from "./components/Footer/footer";
+import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Eventss from "./pages/Eventss/Eventss";
